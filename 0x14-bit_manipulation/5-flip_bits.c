31c45226676a52d0291679dc94a864627d376692
kwetey-nartey <iamkwetey2014@gmail.com>
2023-08-05 06:12:00 -0700

    5-flip_bits.c

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * flip_bits - Count the number of bits that need to be flipped to transform
+ * one number into another
+ * @n: First number
+ * @m: Second number
+ *
+ * Return: Number of bits to be flipped
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int i, count = 0;
+	unsigned long int current;
+	unsigned long int exclusive = n ^ m;
+
+	for (i = 63; i >= 0; i--)
+	{
+		current = exclusive >> i;
+		if (current & 1)
+			count++;
+	}
+
+	return (count);
+}
